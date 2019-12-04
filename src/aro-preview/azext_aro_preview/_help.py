a1e06a820bd44d908011dd140e806ce0427c3460
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2019-12-04 12:23:41 +0000

    init

---
+++ src/aro-preview/azext_aro_preview/_help.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from knack.help_files import helps  # pylint: disable=unused-import
+
+
+helps['aro-preview'] = """
+    type: group
+    short-summary:  Manage Azure Red Hat OpenShift service.
+"""
+
+helps['aro-preview create'] = """
+    type: command
+    short-summary: Create a aro-preview.
+"""
+
+helps['aro-preview list'] = """
+    type: command
+    short-summary: List aro-previews.
+"""
+
+helps['aro-preview delete'] = """
+    type: command
+    short-summary: Delete a aro-preview.
+"""
+
+helps['aro-preview show'] = """
+    type: command
+    short-summary: Show details of a aro-preview.
+"""
+
+helps['aro-preview update'] = """
+    type: command
+    short-summary: Update a aro-preview.
+"""
--- src/aro/README.rst
+++
@@ -1,5 +0,0 @@
-Microsoft Azure CLI 'aro' Extension
-==========================================
-
-This package is for the 'aro' extension.
-i.e. 'az aro'
